6d6b01919ea1d3028159991a4418418b051528e9
Med-dja <m.brahimi.mi@lagh-univ.dz>
2024-05-27 00:45:17 +0100

    finally

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../../components/Header";
+import Header from "../../components/header";
 import HeroSection from "../../components/HeroSection";
 import BookingForm from "../../components/BookingForm";
 import Menu from "../../components/Menu";
